ddd3bf1510cb026ce8b041aae8e9dff390f01de1
Estrela <mcmo@protonmail.com>
2025-03-13 15:17:23 -0300

    ambiente e robo

--- lab1/Ambiente.java
+++ lab1/Ambiente.java
@@ -3,15 +3,20 @@ public class Ambiente{
     private int alturaY;
     private int larguraX;
 
-    public defineAmb(int alturaY, int larguraX){
+    public Ambiente(int alturaY, int larguraX){
+
         this.alturaY = alturaY;
         this.larguraX = larguraX;
     }
 
 
-    public boolean dentroDosLimites (int alturaY, int larguraX){
-        return 
+    public boolean XdentroDosLimites (int alturaY, int posicaoY){
+        return (posicaoY <= alturaY);
 
+    }
+
+    public boolean YdentroDosLimites (int larguraX, int posicaoX){
+        return (larguraX <= posicaoX);
     }
 
 }
