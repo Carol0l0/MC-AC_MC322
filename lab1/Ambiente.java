c6705fdce74d86f8ce5c77374cb945e9b999da40
Estrela <mcmo@protonmail.com>
2025-03-13 14:59:43 -0300

    classe Robo

--- lab1/Ambiente.java
+++ lab1/Ambiente.java
@@ -3,8 +3,7 @@ public class Ambiente{
     private int alturaY;
     private int larguraX;
 
-    public Ambiente(int alturaY, int larguraX){
-
+    public defineAmb(int alturaY, int larguraX){
         this.alturaY = alturaY;
         this.larguraX = larguraX;
     }
--- lab1/Robo.java
+++ lab1/Robo.java
@@ -1,17 +1,23 @@
 public class Robo{
     
-    
-    private String nomeRobo;
+    private String nome;
     private int posicaoX;
     private int posicaoY;
 
 
-    public Robo(String nomeRobo, int posicaoX, int posicaoY){
-
-        this.nomeRobo = nomeRobo;
+    public void construtor(String nome, int posicaoX, int posicaoY){
+        this.nome = nome;
         this.posicaoX = posicaoX;
         this.posicaoY = posicaoY;
+    }
 
+    public void mover(int deltaX, int deltaY){
+        this.posicaoX+=deltaX;
+        this.posicaoY+=deltaY;
+    }
+
+    public String exibirPosicao(){
+        return this.posicaoX+" "+this.posicaoY;
     }
     
 }
