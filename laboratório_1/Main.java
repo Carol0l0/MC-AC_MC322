36316aef8651d7ba28f477a3ca46a145cb2232d0
Ana Carolina <a248734@dac.unicamp.br>
2025-03-13 11:40:26 -0300

    inicial

---
+++ laboratório_1/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class main {
+    public static void main (String[] args) {
+
+        Scanner nomeRobo = new Scanner (System.in);
+        Scanner posicaoX = new Scanner (System.in);
+        Scanner posicaoY = new Scanner (System.in);
+
+
+        System.out.println("Informe o nome do robo: ");
+        nomeRobo = lerNext();
+
+        System.out.println("Informe o tamanho X inicial do tabuleiro: ");
+        posicaoX = lerNextInt();
+
+        System.out.println("Informe o tamanho Y inicial do tabuleiro");
+        posicaoY = lerNextInt ();
+
+
+
+
+
+    }
+
+}
