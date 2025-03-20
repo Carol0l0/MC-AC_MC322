15aab4d06bbae18e86779c13b0d8b0f9bbc3c57d
Estrela <mcmo@protonmail.com>
2025-03-20 14:06:24 -0300

    robo terrestre

---
+++ lab2/Terrestre.java
@@ -0,0 +1,18 @@
+public class Terrestre extends Robo {
+    
+    int v_max;
+    
+    public Terrestre (String nome, int posicaoX, int posicaoY, int v_max){
+        super(nome, posicaoX, posicaoY);
+        this.v_max=v_max;
+    }
+
+    @Override
+    public boolean mover(int deltaX, int deltaY, Ambiente a) {
+        if(deltaX+deltaY>this.v_max){
+            return false;
+        }
+        
+        return super.mover(deltaX, deltaY, a);
+    }
+}
