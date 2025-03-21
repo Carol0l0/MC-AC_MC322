9e2d03cd39160df9c11a6b1f80e4aec915b654f2
Ana Carolina V A <248734@dac.unicamp.br>
2025-03-20 22:58:32 -0300

    adc atributo do terrestre carga de bateria

--- lab2/TerrestreDeCarga.java
+++ lab2/TerrestreDeCarga.java
@@ -1,12 +1,14 @@
 public class TerrestreDeCarga extends Terrestre {
 
     private int cargaMaxima; 
-    private int cargaAtual;  
+    private int cargaAtual; 
+    private int nivelBateria; 
 
-    public TerrestreDeCarga(String nome, int posicaoX, int posicaoY, int posicaoZ, int v_max, int cargaMaxima) {
+    public TerrestreDeCarga(String nome, int posicaoX, int posicaoY, int posicaoZ, int v_max, int cargaMaxima, int bateria) {
         super(nome, posicaoX, posicaoY, posicaoZ, v_max);
         this.cargaMaxima = cargaMaxima;
         this.cargaAtual = 0;
+        this.nivelBateria = 100;
     }
 
     public boolean carregar(int peso) {
@@ -20,11 +22,28 @@ public class TerrestreDeCarga extends Terrestre {
         }
     }
 
-    public int getCargaAtual() {
-        return cargaAtual;
+    @Override
+    public boolean mover(int deltaX, int deltaY, Ambiente a) {
+        boolean conseguiuMover = super.mover(deltaX, deltaY, a);
+    
+        if (!conseguiuMover) {
+            System.out.println(getNome() + " nao conseguiu se mover!");
+            return false;
+        }
+    
+        if (nivelBateria >= 10) {
+            nivelBateria -= 10;
+        } else {
+            nivelBateria = 0;
+            System.out.println(getNome() + " esta sem bateria! Precisa recarregar.");
+        }
+    
+        System.out.println(getNome() + " Posicao: (" + posicaoX + ", " + posicaoY + "). Nivel de bateria: " + nivelBateria + "%");
+    
+        return true;
     }
     
-    public int getCargaMaxima() {
-        return cargaMaxima;
-    }
+
 }
+
+
